0caefd9507fd7e674f39e415f8aeb4d56b2a5a30
Diego Ledesma <diegoledesma23@gmail.com>
2022-10-23 20:24:07 -0300

    ultimo de js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const boton = document.querySelectorAll('.btn');
-console.log(boton);
-console.log(boton[1]);
-
-const dato = document.getElementById("nombre");
-console.log(dato);
-
 
 //////////////////////////////////////////////////
 const animal = {
@@ -12,9 +5,9 @@ const animal = {
     duenio: "Diego Ledesma",
     raza: "Galgo",
     edad: 4,
-    actividad: ["correr","jugar","hace pozos"],
-    comida: ["balanceada","carne","verduras"],
-    visita: "01/08/2022",
+    actividad: ["Correr"," jugar"," hace pozos"],
+    comida: ["Balanceada"," carne"," verduras"],
+    visita: "Ultima visita: 01/08/2022",
     vacuna: "Esquema Completo"
 }  
 
@@ -32,3 +25,91 @@ console.log("Visita al Veterinario: ", animal.visita);
 console.log("vacuna: ", animal.vacuna);
 
 
+const botonNombre = document.querySelector(".nombre");
+botonNombre.addEventListener("click", function(){
+    let tituloNombre = botonNombre.textContent;
+    if(tituloNombre == "Nombre de tu Mascota"){
+        botonNombre.textContent =  animal.nombre;
+    } else {
+        tituloNombre = "Nombre de tu Mascota";
+        botonNombre.textContent =  tituloNombre;
+    }
+});
+
+const botonDuenio = document.querySelector(".duenio");
+botonDuenio.addEventListener("click", function(){
+    let tituloDuenio = botonDuenio.textContent;
+    if(tituloDuenio == "Dueño/a de Mascota"){
+        botonDuenio.textContent =  animal.duenio;
+    } else {
+        tituloDuenio = "Dueño/a de Mascota";
+        botonDuenio.textContent =  tituloDuenio;
+    }
+});
+
+const botonRaza = document.querySelector(".raza");
+botonRaza.addEventListener("click", function(){
+    let tituloRaza = botonRaza.textContent;
+    if(tituloRaza == "Raza"){
+        botonRaza.textContent = animal.raza;
+    }else{
+        tituloRaza = "Raza";
+        botonRaza.textContent = tituloRaza;
+    }
+})
+
+const botonEdad = document.querySelector(".edad");
+botonEdad.addEventListener("click", function(){
+    let tituloEdad = botonEdad.textContent;
+    if(tituloEdad == "Edad"){
+        botonEdad.textContent = animal.edad;
+    }else{
+        tituloEdad = "Edad";
+        botonEdad.textContent = tituloEdad;
+    }
+})
+
+const botonActividades = document.querySelector(".actividad");
+botonActividades.addEventListener("click", function(){
+    let tituloActividad = botonActividades.textContent;
+    if(tituloActividad == "Actividades"){
+        botonActividades.textContent = animal.actividad;
+    }else{
+        tituloActividad = "Actividades";
+        botonActividades.textContent = tituloActividad;
+    }
+})
+
+
+const botonComida = document.querySelector(".comida");
+botonComida.addEventListener("click", function(){
+    let tituloComida = botonComida.textContent;
+    if(tituloComida == "Comida"){
+        botonComida.textContent = animal.comida;
+    }else{
+        tituloComida = "Comida";
+        botonComida.textContent = tituloComida;
+    }
+})
+
+const botonVisita = document.querySelector(".visita");
+botonVisita.addEventListener("click", function(){
+    let tituloVisita = botonVisita.textContent;
+    if(tituloVisita == "Visita al veterinario"){
+        botonVisita.textContent = animal.visita;
+    }else{
+        tituloVisita = "Visita al veterinario";
+        botonVisita.textContent = tituloVisita;
+    }
+})
+
+const botonVacunas = document.querySelector(".vacuna");
+botonVacunas.addEventListener("click", function(){
+    let tituloVisita = botonVacunas.textContent;
+    if(tituloVisita == "Vacunas"){
+        botonVacunas.textContent = animal.vacuna;
+    }else{
+        tituloVisita = "Vacunas";
+        botonVacunas.textContent = tituloVisita;
+    }
+})
